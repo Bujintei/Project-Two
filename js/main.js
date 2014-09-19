afb8f29cd8302269abeb935e736a4c6ff4ad4ed7
Brandon Nguyen <eagleeye2555@gmail.com>
2014-09-19 01:35:12 -0700

    Added bootstrap to my project and also decided to try out media queries. So far so good but this is more of a test anyway and I'll make it better in the next push.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+<!-- hide from none JavaScript Browsers
+Image1= new Image(304,228)
+Image1.src = "http://img3.wikia.nocookie.net/__cb20140629001242/disneycreate/images/7/75/Over_9000.gif"
+Image2 = new Image(304,228)
+Image2.src = "https://lh5.googleusercontent.com/-K7qNvzoaBGk/AAAAAAAAAAI/AAAAAAAAAAA/Qna2mpJNS7Y/s600-c-k-no/photo.jpg?feature=og"
+function SwapOut() {
+document.imageflip.src = Image2.src; return true;
+}
+function SwapBack() {
+document.imageflip.src = Image1.src; return true;
+}
+// - stop hiding -->
